438805bf70c815693b7f8ba0798c89e433e4cb2a
Tom Ecrepont <tomecrepont@gmail.com>
2023-01-24 08:32:05 +0100

    add style

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      'blue': '#118AB2',
+      'background-dark': '#073B4C',
+      'background-light': '#f7fff7',
+      'red': '#EF476F',
+      'yellow': '#FFD166',
+      'green': '#06D6A0',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
@@ -20,3 +28,4 @@ module.exports = {
     require('@tailwindcss/typography'),
   ]
 }
+
