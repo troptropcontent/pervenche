dc4f4d9a8ee20101f203bebf215755ca4196d788
Tom Ecrepont <tomecrepont@gmail.com>
2023-01-24 08:32:16 +0100

    add assocxiation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-:omniauthable, omniauth_providers: %i[google_oauth2]
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 
+  has_many :services
+  has_many :robots, through: :services
 
   def self.from_omniauth(auth)
-      find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-          user.email = auth.info.email
-          user.password = Devise.friendly_token[0, 20]
-      end
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
   end
 end
