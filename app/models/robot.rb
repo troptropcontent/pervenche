7443346869b8af3cef77096a846a60f17732bb6a
Tom Ecrepont <tomecrepont@gmail.com>
2023-01-25 09:01:03 +0100

    allow robot to retrieve a current ticket at client s

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,4 +2,23 @@ class Robot < ApplicationRecord
   belongs_to :service
   encrypts :license_plate, :payment_method, :zipcode
   validates_presence_of :name, :license_plate, :payment_method, :zipcode
+
+  def current_ticket
+    client.current_ticket
+  end
+
+  private
+
+  def client
+    @client ||= ParkingTicket::Base.new(
+      service.kind,
+      {
+        username: service.username,
+        password: service.password,
+        license_plate: license_plate,
+        zipcode: zipcode,
+        card_number: payment_method
+      }
+    )
+  end
 end
