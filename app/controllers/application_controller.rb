2f587d42916dd4206cff16111d72cbdb6555368c
Tom Ecrepont <tomecrepont@gmail.com>
2023-02-08 18:26:02 +0100

    catch errors properly for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  rescue_from ActiveRecord::RecordNotFound do |_exception|
+    not_found
+  end
+  rescue_from CanCan::AccessDenied do |_exception|
+    head :forbidden
+  end
+
   def not_found
     raise ActionController::RoutingError, 'Not Found'
   end
