05181b0a9b3791f8da6fc6597372df58a691f65b
Tom Ecrepont <tomecrepont@gmail.com>
2023-02-08 18:29:00 +0100

    allow GET /automated_tickets/new to redirect to the wizard

--- app/controllers/automated_tickets_controller.rb
+++ app/controllers/automated_tickets_controller.rb
@@ -1,4 +1,7 @@
 class AutomatedTicketsController < ApplicationController
   def new
+    automated_ticket = current_user.automated_tickets.new
+    automated_ticket.save(validate: false)
+    redirect_to automated_ticket_setup_path(automated_ticket.id, AutomatedTicket.setup_steps.keys.first)
   end
 end
