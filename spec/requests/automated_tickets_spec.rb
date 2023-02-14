0db3b54322bc8d1dd7a01e48a8a1f5eb9bceb6e1
Tom Ecrepont <tomecrepont@gmail.com>
2023-02-14 08:49:11 +0100

    update html erb files

--- spec/requests/automated_tickets_spec.rb
+++ spec/requests/automated_tickets_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "AutomatedTickets", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/automated_tickets/new"
+RSpec.describe 'AutomatedTickets', type: :request do
+  describe 'GET /automated_tickets/new' do
+    it 'returns http success' do
+      get '/automated_tickets/new'
       expect(response).to have_http_status(:success)
     end
   end
-
+  describe 'GET /automated_tickets/new' do
+    it 'returns http success' do
+      get '/automated_tickets/new'
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'PUT /automated_tickets' do
+    it 'returns http success' do
+      get '/automated_tickets/new'
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
