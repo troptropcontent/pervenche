2723ff7452e3795ea85e61e0de7325c63b244bdb
Tom Ecrepont <tomecrepont@gmail.com>
2023-02-15 09:11:24 +0100

    add a running ticket to service

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,11 +3,12 @@ class Service < ApplicationRecord
   has_many :robots
   encrypts :username, :password
   enum :kind, {
-    pay_by_phone: 0
+    pay_by_phone: 0,
+    easy_park: 1
   }
 
   validates :username, uniqueness: { scope: :kind }
-  validates_presence_of :name, :username, :password
+  validates_presence_of :kind, :username, :password
   validate :valid_credentials
 
   def vehicles
@@ -29,10 +30,17 @@ class Service < ApplicationRecord
     end
   end
 
+  def running_ticket(license_plate, zipcode)
+    Rails.cache.fetch("#{cache_key_with_version}/#{kind}/#{username}/#{license_plate}/#{zipcode}/running_ticket",
+                      expires_in: 1.minutes) do
+      client.running_ticket(license_plate, zipcode)
+    end
+  end
+
   private
 
   def valid_credentials
-    return if ParkingTicket::Base.valid_credentials?(kind, username, password)
+    return if kind && ParkingTicket::Base.valid_credentials?(kind, username, password)
 
     errors.add(:credentials, I18n.t('models.service.validations.credentials'))
   end
