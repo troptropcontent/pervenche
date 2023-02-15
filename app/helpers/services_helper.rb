6a2046f5cb316bcea19a0398df2be6140139d6b0
Tom Ecrepont <tomecrepont@gmail.com>
2023-02-15 09:10:05 +0100

    allow the kinds helper to receive a disabled argument

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,7 +1,9 @@
 module ServicesHelper
-  def kinds
+  def kinds(disabled: [])
     Service.kinds.each_with_object([]) do |(key, _value), memo|
-      memo << [t("models.service.attributes.kind.enum.#{key}"), key]
+      base_data = [t("models.service.attributes.kind.enum.#{key}"), key]
+      options = { disabled: true } if disabled.include?(key.to_sym)
+      memo << [*base_data, options].compact
     end
   end
 end
