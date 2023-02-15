c1263702f3b44cdb08b19e46bd3222a6501f8faf
Tom Ecrepont <tomecrepont@gmail.com>
2023-02-15 09:09:39 +0100

    update service controller to assgn a name if not provided

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,6 +4,7 @@ class ServicesController < ApplicationController
 
   def create
     if @service.save
+      assign_default_name unless service_params[:name]
       redirect_to root_path
     else
       render 'new', status: :unprocessable_entity
@@ -15,6 +16,11 @@ class ServicesController < ApplicationController
   private
 
   def service_params
-    params.require(:service).permit(:username, :password, :name, :kind)
+    params.require(:service).permit(:username, :password, :name, :kind).compact_blank
+  end
+
+  def assign_default_name
+    @service.name = "#{t("models.service.attributes.kind.enum.#{@service.kind}")} - #{@service.username}"
+    @service.save
   end
 end
