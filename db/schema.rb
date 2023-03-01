f1eae0371fd282a5cfc1476c5339fef28ce3b046
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-01 09:13:26 +0100

    run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_185613) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_071442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_185613) do
 
   create_table "ticket_requests", force: :cascade do |t|
     t.bigint "automated_ticket_id", null: false
-    t.string "payment_method_client_internal_id", null: false
+    t.string "payment_method_client_internal_id"
     t.datetime "requested_on", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
