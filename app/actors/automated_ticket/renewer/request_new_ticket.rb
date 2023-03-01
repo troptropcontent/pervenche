0af99faa5561f430e456f2a4c17a2d709dd95108
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-01 11:20:34 +0100

    create AutomatedTicket::Renewer::RequestNewTicket and its specs

--- app/actors/automated_ticket/renewer/request_new_ticket.rb
+++ app/actors/automated_ticket/renewer/request_new_ticket.rb
@@ -1,6 +1,34 @@
 # frozen_string_literal: true
 
 class AutomatedTicket::Renewer::RequestNewTicket < Actor
+  input :automated_ticket
+  input :payment_method_id
+  input :time_unit
+  input :quantity
+  output :ticket_request
+
   def call
+    request_new_ticket!
+    save_ticket_request!
+  end
+
+  private
+
+  def request_new_ticket!
+    automated_ticket.renew!(
+      quantity: quantity, 
+      time_unit: time_unit, 
+      payment_method_client_internal_id: payment_method_id
+    )
+  end
+
+  def save_ticket_request!
+    self.ticket_request = TicketRequest.create!({
+      automated_ticket_id: automated_ticket.id,
+      payment_method_client_internal_id: payment_method_id,
+      requested_on: Time.current,
+      quantity: quantity, 
+      time_unit: time_unit}
+    )
   end
 end
--- spec/actors/automated_ticket/renewer/request_new_ticket_spec.rb
+++ spec/actors/automated_ticket/renewer/request_new_ticket_spec.rb
@@ -1,9 +1,33 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
+require 'support/shared_context/a_user_with_a_service_with_an_automated_ticket'
 
 RSpec.describe AutomatedTicket::Renewer::RequestNewTicket, type: :actor do
+  include_context 'a user with a service with an automated ticket'
+  subject {described_class.call(automated_ticket:, payment_method_id:, time_unit:, quantity:)}
+  let(:payment_method_id) {'AFakePaymentMethodId'}
+  let(:time_unit) {'days'}
+  let(:quantity) {1}
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "requests a new ticket and saves the request in the database" do
+      expect(automated_ticket).to receive(:renew!).with(
+        quantity: , 
+        time_unit: , 
+        payment_method_client_internal_id: payment_method_id
+      )
+      ticket_request_count = TicketRequest.count
+      subject  
+      last_ticket_request_saved = TicketRequest.last
+      expect(TicketRequest.count).to eq(ticket_request_count + 1)
+      expect(last_ticket_request_saved.automated_ticket_id).to eq(automated_ticket.id)
+      expect(last_ticket_request_saved.payment_method_client_internal_id).to eq('AFakePaymentMethodId')
+      expect(last_ticket_request_saved.time_unit).to eq('days')
+      expect(last_ticket_request_saved.quantity).to eq(1)
+      expect(last_ticket_request_saved.requested_on).to be_within(0.1).of(Time.now)
+    end
   end
 end
+
+
+
