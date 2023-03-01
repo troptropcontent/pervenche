1aff4b8bcc1dd578aad6c08e5a2c486af868c1a8
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-01 10:44:00 +0100

    add this method to the renewer condition

--- app/actors/automated_ticket/renewer.rb
+++ app/actors/automated_ticket/renewer.rb
@@ -6,6 +6,6 @@ class AutomatedTicket::Renewer < Actor
     play FindPaymentMethod, 
          FindTimeUnitAndQuantity,
          RequestNewTicket, 
-         if: -> actor { actor.running_ticket.nil?}
+         if: -> actor { actor.running_ticket.nil? && actor.automated_ticket.should_renew_today? }
   end
 end
