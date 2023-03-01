1b4ae431ea99577e6288534561ee7bc7e281af3a
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-01 10:30:14 +0100

    create the first step of the actor

--- app/actors/automated_ticket/renewer/find_or_save_running_ticket.rb
+++ app/actors/automated_ticket/renewer/find_or_save_running_ticket.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class AutomatedTicket::Renewer::FindOrSaveRunningTicket < Actor
+  input :automated_ticket
+  output :running_ticket
   def call
+    return if self.running_ticket = self.automated_ticket.running_ticket_in_database 
+    running_ticket_in_client = self.automated_ticket.running_ticket_in_client
+    if running_ticket_in_client
+      self.running_ticket = self.automated_ticket.tickets.create(running_ticket_in_client.except(:client))
+    else
+      self.running_ticket = nil
+    end
   end
 end
