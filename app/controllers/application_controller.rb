4d032d68238bbf81ddeed43c32777e4972f92e4a
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-02 20:32:28 +0100

    show navbar only when required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :require_operationnal, if: :operationnal_controller?
+  before_action :require_navbar
 
   rescue_from ActiveRecord::RecordNotFound do |_exception|
     not_found
@@ -22,5 +23,9 @@ class ApplicationController < ActionController::Base
   def operationnal_controller?
     !devise_controller? && %w[onboardings
                               setups].exclude?(controller_name) && %w[new create update].exclude?(action_name)
+  end
+
+  def require_navbar
+    @with_navbar = true
   end
 end
