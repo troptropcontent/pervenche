73032335629c40f611c119ff7dcd4d31293edb00
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-05 20:26:46 +0100

    generate setup controller

---
+++ app/controllers/setup_controller.rb
@@ -0,0 +1,22 @@
+class SetupController < ApplicationController
+  def service
+  end
+
+  def localisation
+  end
+
+  def vehicle
+  end
+
+  def zipcode
+  end
+
+  def rate_options
+  end
+
+  def weekdays
+  end
+
+  def payment_methods
+  end
+end
