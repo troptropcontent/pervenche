598e9935fb8cf51c8a6f972a933026bb37d6fa77
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-08 06:54:41 +0100

    remove unwanted controllers

--- spec/views/setup/vehicle.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "setup/vehicle.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/setup/weekdays.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "setup/weekdays.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
