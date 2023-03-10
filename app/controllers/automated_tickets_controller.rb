2e4c2d489edbad8ff1471b2dfc58493fb7efd947
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-10 12:07:24 +0100

    automaticaly set localisation to paris for now

--- app/controllers/automated_tickets_controller.rb
+++ app/controllers/automated_tickets_controller.rb
@@ -3,6 +3,7 @@ class AutomatedTicketsController < ApplicationController
   def new
     automated_ticket = current_user.automated_tickets.new
     automated_ticket.service = current_user.services.first if current_user.services.count == 1
+    automated_ticket.localisation = 'paris'
     automated_ticket.save(validate: false)
     redirect_to automated_ticket_setup_path(automated_ticket.id, AutomatedTicket.setup_steps.keys.first)
   end
