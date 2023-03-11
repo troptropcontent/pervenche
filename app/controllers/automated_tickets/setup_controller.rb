5c3f5b82277ff4da6b287d9e1e1b409ded8e48b3
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-11 13:03:54 +0100

    update setup controller

--- app/controllers/automated_tickets/setup_controller.rb
+++ app/controllers/automated_tickets/setup_controller.rb
@@ -1,5 +1,6 @@
 class AutomatedTickets::SetupController < ApplicationController
   ALLOWED_VEHICLE_TYPES = ['electric_motorcycle']
+  ALLOWED_LOCALISATIONS = ['paris']
   ARRAY_FIELDS = %i[weekdays accepted_time_units zipcodes payment_method_client_internal_ids].freeze
   before_action :load_automated_ticket
   before_action :load_step!
@@ -44,7 +45,7 @@ class AutomatedTickets::SetupController < ApplicationController
   end
 
   def redirect_to_next_step!
-    redirect_to automated_ticket_setup_path(@automated_ticket.id, next_step)
+    redirect_to path_for(step: next_step)
   end
 
   def step_already_completed?
@@ -55,17 +56,56 @@ class AutomatedTickets::SetupController < ApplicationController
 
   def load_data_for(step:)
     load_vehicle_step_data if step == 'vehicle'
+    load_zipcodes_step_data! if step == 'zipcodes'
+    load_rate_option_step_data! if step == 'rate_option'
   end
 
   def load_vehicle_step_data
     @vehicles = @automated_ticket.service.vehicles.filter {|vehicle| ALLOWED_VEHICLE_TYPES.include?(vehicle[:vehicle_type])}
   end
+
+  def load_zipcodes_step_data!
+    if ALLOWED_LOCALISATIONS.include?(setup_params[:localisation])
+      @localisation = setup_params[:localisation]
+    else
+      not_found
+    end
+  end
+
+  def load_rate_option_step_data!
+    @rate_options = find_rate_options_shared_between_zipcodes
+  end
+
+  def find_rate_options_shared_between_zipcodes
+    byebug
+    @automated_ticket.zipcodes.map
+  end
+
   def permited_params_for_step
     params.require(:automated_ticket).permit(permitted_fields_for_step)
   end
+
   def permitted_fields_for_step
     AutomatedTicket.setup_steps[@step.to_sym].map do |field|
       ARRAY_FIELDS.include?(field) ? { field => [] } : field
     end
+  end
+
+  def path_for(step:)
+    params = base_params(step: step).merge(query_params_for(step: step))
+    automated_ticket_setup_path(**params)
+  end
+
+  def query_params_for(step:)
+    return {localisation: @automated_ticket.localisation} if step.to_sym == :zipcodes
+    {}
+  end
+
+  def base_params(step:)
+    {automated_ticket_id: @automated_ticket.id, step_name: step}
+  end
+
+  def setup_params
+    params.permit(:localisation)
   end
 end
