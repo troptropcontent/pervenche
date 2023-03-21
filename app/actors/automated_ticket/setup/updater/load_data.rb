81c700f9b56950629553c32720e356c12ba92d50
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-21 21:16:59 +0100

    create AutomatedTicket::Setup::Updater::LoadData and its specs

--- app/actors/automated_ticket/setup/updater/load_data.rb
+++ app/actors/automated_ticket/setup/updater/load_data.rb
@@ -1,6 +1,41 @@
 # frozen_string_literal: true
 
 class AutomatedTicket::Setup::Updater::LoadData < Actor
+  input :automated_ticket
+  input :step
+  output :data
+  output :next_step
   def call
+    self.data = {}
+    load_data_required_for_service_step if step == :service
+    load_data_required_for_vehicle_step if step == :vehicle
+    load_data_required_for_rate_option_step if step == :rate_option
+    load_data_required_for_payment_methods_step if step == :payment_methods
+  end
+
+  private
+
+  def load_data_required_for_service_step
+    self.data = {
+      services: automated_ticket.user.services.pluck(:name, :id)
+    }
+  end
+
+  def load_data_required_for_vehicle_step
+    self.data = {
+      vehicles: automated_ticket.service.vehicles
+    }
+  end
+
+  def load_data_required_for_rate_option_step
+    self.data = {
+      rate_options: automated_ticket.rate_options_shared_between_zipcodes
+    }
+  end
+
+  def load_data_required_for_payment_methods_step
+    self.data = {
+      payment_methods: automated_ticket.service.payment_methods
+    }
   end
 end
--- spec/actors/automated_ticket/setup/updater/load_data_spec.rb
+++ spec/actors/automated_ticket/setup/updater/load_data_spec.rb
@@ -1,9 +1,66 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
+require 'support/shared_context/a_user_with_a_service_with_an_automated_ticket'
+require 'support/shared_context/service_stubs'
 
 RSpec.describe AutomatedTicket::Setup::Updater::LoadData, type: :actor do
+  include_context 'a user with a service with an automated ticket', :payment_methods
+  include_context 'a stubed service'
+  let(:step) { :service }
+  subject { described_class.call(automated_ticket:, step:) }
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    describe 'step' do
+      context 'service' do
+        let(:expected_data) do
+          { services: [[service.name, service.id]] }
+        end
+        it 'loads the correct data' do
+          expect(subject.data).to eq(expected_data)
+        end
+      end
+      context 'vehicle' do
+        let(:step) { :vehicle }
+        let(:expected_data) do
+          { vehicles: [
+            { client_internal_id: 'a_fake_client_id',
+              license_plate: 'a_fake_license_plate',
+              vehicle_description: 'a_fake_name',
+              vehicle_type: 'electric_motorcycle' }
+          ] }
+        end
+        it 'loads the correct data' do
+          expect(subject.data).to eq(expected_data)
+        end
+      end
+      context 'rate_option' do
+        let(:step) { :rate_option }
+        let(:expected_data) do
+          { rate_options: [
+            { accepted_time_units: %w[minutes hours],
+              client_internal_id: '1085252721',
+              free: true,
+              name: 'shared rate option',
+              type: 'CUSTOM' }
+          ] }
+        end
+        it 'loads the correct data' do
+          expect(subject.data).to eq(expected_data)
+        end
+      end
+      context 'payment_methods' do
+        let(:step) { :payment_methods }
+        let(:expected_data) do
+          { payment_methods: [
+            { anonymised_card_number: '6156',
+              client_internal_id: 'fake-payment-methof-id-9654e1084eb1',
+              payment_card_type: 'visa' }
+          ] }
+        end
+        it 'loads the correct data' do
+          expect(subject.data).to eq(expected_data)
+        end
+      end
+    end
   end
 end
