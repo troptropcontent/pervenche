05fd56fdf1fcf8989b6b882532bef177af5f09aa
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-26 13:03:41 +0200

    update FindPaymentMethod to work with the new system

--- spec/actors/automated_ticket/renewer/find_payment_method_spec.rb
+++ spec/actors/automated_ticket/renewer/find_payment_method_spec.rb
@@ -6,15 +6,17 @@ RSpec.describe AutomatedTicket::Renewer::FindPaymentMethod, type: :actor do
   include_context 'a user with a service with an automated ticket'
   subject { described_class.call(automated_ticket:) }
   describe '.call' do
-    context 'when the automated_ticket.payment_method_client_internal_id is free' do
-      let(:automated_ticket_payment_method_client_internal_id) { 'free' }
+    context 'when the automated_ticket is free' do
+      let(:automated_ticket_free) { true }
       it 'should assigns actor.payment_method_id to nil' do
         expect(subject.payment_method_id).to eq(nil)
       end
     end
-    context 'when the automated_ticket.payment_method_client_internal_id is not free' do
-      it 'should assigns actor.payment_method_id to payment_method_client_internal_id' do
-        expect(subject.payment_method_id).to eq(automated_ticket.payment_method_client_internal_id)
+    context 'when the automated_ticketis not free' do
+      let(:automated_ticket_free) { false }
+      let(:payment_method_client_internal_ids) { ['the_first_payment_id', 'the_second_payment_method_id'] }
+      it 'should assigns actor.payment_method_id to the first payment_method_client_internal_id' do
+        expect(subject.payment_method_id).to eq('the_first_payment_id')
       end
     end
   end
