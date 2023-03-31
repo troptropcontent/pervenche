c61d018e7544d6f2f6319b79a999cb8f6985a67b
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-31 13:00:21 +0200

    add a validation on the uniqueness of username for a service kind

--- db/migrate/20230331101615_add_unique_index_on_username_and_kind_in_services.rb
+++ db/migrate/20230331101615_add_unique_index_on_username_and_kind_in_services.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexOnUsernameAndKindInServices < ActiveRecord::Migration[7.0]
   def change
-    add_index :services, [:username, :kind], unique: true
+    add_index :services, %i[username kind], unique: true
   end
 end
