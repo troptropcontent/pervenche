9c222229128413534f647d77367ea297d80f1d75
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-31 15:39:24 +0200

    constraint similar ticket already registered to check ready automated ticket only (#11)
    * only check similar ticket in ready tickets
    
    * only check similar ticket in ready tickets

--- spec/actors/automated_tickets/find_similar_ticket_spec.rb
+++ spec/actors/automated_tickets/find_similar_ticket_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe AutomatedTickets::FindSimilarTicket, type: :actor do
   describe '.call' do
     context 'when a automated_ticket already covers on of the zipcodes' do
       let!(:automated_ticket_already_persisted) do
-        FactoryBot.create(:automated_ticket, :set_up, user:, service:, zipcodes: ['75018'])
+        FactoryBot.create(:automated_ticket, :set_up, user:, service:, zipcodes: ['75018'], status:)
       end
+      let(:status) { :ready }
       let(:user) { FactoryBot.create(:user) }
       let(:service) do
         service = FactoryBot.build(:service, user_id: user.id)
@@ -17,9 +18,18 @@ RSpec.describe AutomatedTickets::FindSimilarTicket, type: :actor do
       let(:automated_ticket) do
         FactoryBot.build(:automated_ticket, :set_up, user:, service:, zipcodes: %w[75018 75017])
       end
-      it 'returns the already covered zipcodes' do
-        result = described_class.call(automated_ticket:)
-        expect(result.zipcodes_already_covered).to eq(['75018'])
+      context 'already persisted ticket ready' do
+        it 'returns the already covered zipcodes' do
+          result = described_class.call(automated_ticket:)
+          expect(result.zipcodes_already_covered).to eq(['75018'])
+        end
+      end
+      context 'already persisted ticket is not ready' do
+        let(:status) { :started }
+        it 'returns the already covered zipcodes' do
+          result = described_class.call(automated_ticket:)
+          expect(result.zipcodes_already_covered).to eq([])
+        end
       end
     end
   end
