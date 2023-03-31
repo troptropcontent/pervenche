e18d6d25ccffb311dfc0ed387d8c5aa147abb696
Tom Ecrepont <tomecrepont@gmail.com>
2023-03-31 14:49:41 +0200

    create traits for automated_tickets

--- spec/factories/automated_tickets.rb
+++ spec/factories/automated_tickets.rb
@@ -1,13 +1,45 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :automated_ticket do
     user
     service
-    type {}
-    rate_option_client_internal_id { 'MyString' }
-    license_plate { 'MyString' }
-    zipcode { 'MyString' }
-    weekdays { [6] }
-    accepted_time_units { ['days'] }
-    payment_method_client_internal_id { 'MyString' }
+    type { 'pay_by_phone' }
+
+    trait :with_localisation do
+      localisation { 'paris' }
+    end
+
+    trait :with_vehicle do
+      license_plate { 'CL12345KK' }
+      vehicle_type { 'electric_motorcycle' }
+    end
+
+    trait :with_zipcodes do
+      zipcodes { %w[75018 75019 75020] }
+    end
+
+    trait :with_rate_option do
+      rate_option_client_internal_id { 'bhkjhlkjhkljhlkjhlkjhkjhkzeizeoiruzeo' }
+      accepted_time_units { ['days'] }
+    end
+
+    trait :with_weekdays do
+      weekdays { [1, 2, 3, 4, 5, 6] }
+    end
+
+    trait :with_payment_methods do
+      payment_method_client_internal_ids { ['rytrtt88ppezoezpeop'] }
+    end
+
+    trait :set_up do
+      with_localisation
+      with_vehicle
+      with_zipcodes
+      with_rate_option
+      with_weekdays
+      with_payment_methods
+      status { :ready }
+    end
   end
 end
