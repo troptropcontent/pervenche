e3272a1bbd3cc3501321eee6b4900ac4835b2906
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-01 11:38:37 +0200

    fix the renewer

--- app/actors/automated_ticket/renewer/find_payment_method.rb
+++ app/actors/automated_ticket/renewer/find_payment_method.rb
@@ -4,6 +4,6 @@ class AutomatedTicket::Renewer::FindPaymentMethod < Actor
   input :automated_ticket
   output :payment_method_id
   def call
-    self.payment_method_id =  automated_ticket.payment_method_client_internal_ids[0] unless automated_ticket.free
+    self.payment_method_id = (automated_ticket.payment_method_client_internal_ids[0] unless automated_ticket.free)
   end
 end
--- spec/actors/automated_ticket/renewer_spec.rb
+++ spec/actors/automated_ticket/renewer_spec.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'support/shared_context/a_user_with_a_service_with_an_automated_ticket'
 
 RSpec.describe AutomatedTicket::Renewer, type: :actor do
-  include_context 'a user with a service with an automated ticket'
-  subject { described_class.call(automated_ticket:) }
+  subject { described_class.call(automated_ticket_id: automated_ticket.id, zipcode: '75016') }
+  let(:automated_ticket) do
+    FactoryBot.create(:automated_ticket, :set_up, user:, service:, zipcodes:, weekdays:, free:, accepted_time_units:)
+  end
+  let(:free) { false }
+  let(:accepted_time_units) { ['days'] }
+  let(:weekdays) { [Date.today.wday] }
+  let(:zipcodes) { %w[75018 75017 75016] }
+  let(:user) { FactoryBot.create(:user) }
+  let(:service) do
+    service = FactoryBot.build(:service, user_id: user.id)
+    service.save(validate: false)
+    service
+  end
+  before do
+    allow(AutomatedTicket).to receive(:find).with(automated_ticket.id).and_return(automated_ticket)
+  end
+
   describe '.call' do
     shared_examples 'it does not request a new ticket' do
       it 'does not request a new ticket' do
@@ -15,16 +30,16 @@ RSpec.describe AutomatedTicket::Renewer, type: :actor do
     end
     context 'when a running ticket is found in the database' do
       let!(:running_ticket_in_database) do
-        FactoryBot.create(:ticket, automated_ticket:, ends_on: Time.current + 2.hours)
+        FactoryBot.create(:ticket, automated_ticket:, zipcode: '75016', ends_on: Time.current + 2.hours)
       end
       it_behaves_like 'it does not request a new ticket'
     end
     context 'when a running ticket is not found in the database' do
       context 'when a running_ticket is found in the client' do
-        let!(:running_ticket_in_client) {}
         before do
-          allow(automated_ticket).to receive(:running_ticket_in_client).and_return(running_ticket_in_client)
+          allow(automated_ticket).to receive(:running_ticket_in_client_for).with({ zipcode: '75016' }).and_return(running_ticket_in_client)
         end
+        let(:running_ticket_in_client) {}
         context 'when a running ticket is found at the client' do
           let(:running_ticket_in_client) do
             { client_internal_id: '34ae093a-37f4-4326-bdbf-7965c82b378a',
@@ -42,17 +57,17 @@ RSpec.describe AutomatedTicket::Renewer, type: :actor do
         end
         context 'when a running ticket is not found at the client' do
           context 'when automated ticket should not be renewed today' do
-            let(:automated_ticket_weekdays) { [Date.today.tomorrow.wday] }
+            let(:weekdays) { [Date.today.tomorrow.wday] }
             it_behaves_like 'it does not request a new ticket'
           end
           context 'when automated_ticket should be renewed today' do
-            let(:automated_ticket_weekdays) { [Date.today.wday] }
             let(:expected_qantity) { 1 }
             let(:expected_time_unit) { 'days' }
-            let(:expected_payment_method_client_internal_id) { 'AFakePaymentMethodId' }
+            let(:expected_payment_method_client_internal_id) { 'rytrtt88ppezoezpeop' }
             shared_examples 'it requests a new ticket' do
-              it 'does not request a new ticket' do
+              it 'it requests a new ticket' do
                 expect(automated_ticket).to receive(:renew!).with(
+                  zipcode: '75016',
                   quantity: expected_qantity,
                   time_unit: expected_time_unit,
                   payment_method_client_internal_id: expected_payment_method_client_internal_id
@@ -63,25 +78,23 @@ RSpec.describe AutomatedTicket::Renewer, type: :actor do
                 expect(subject.ticket_request.time_unit).to eq(expected_time_unit)
                 expect(subject.ticket_request.quantity).to eq(expected_qantity)
                 expect(subject.ticket_request.requested_on).to be_within(0.1).of(Time.now)
+                expect(subject.ticket_request.zipcode).to eq('75016')
               end
             end
-            context 'when automated_ticket.payment_method_client_internal_id is set to free' do
-              let(:automated_ticket_payment_method_client_internal_id) { 'free' }
+            context 'when automated_ticket is free' do
+              let(:free) { true }
               let(:expected_payment_method_client_internal_id) { nil }
               it_behaves_like 'it requests a new ticket'
             end
             context 'when automated_ticket.payment_method_client_internal_id is not set to free' do
-              let(:automated_ticket_payment_method_client_internal_id) { 'a_payment_id' }
-              let(:expected_payment_method_client_internal_id) { 'a_payment_id' }
               it_behaves_like 'it requests a new ticket'
             end
             context 'when automated_ticket.accepted_time_units contains days' do
-              let(:automated_ticket_accepted_time_units) { ['days'] }
               let(:expected_time_unit) { 'days' }
               it_behaves_like 'it requests a new ticket'
             end
             context 'when automated_ticket.accepted_time_units does not contain days' do
-              let(:automated_ticket_accepted_time_units) { ['something_else'] }
+              let(:accepted_time_units) { ['something_else'] }
               let(:expected_time_unit) { 'hours' }
               it_behaves_like 'it requests a new ticket'
             end
