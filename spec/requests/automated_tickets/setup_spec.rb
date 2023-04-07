a15dc3abd73ee5103cc2b5b75a5ef7dbcb156882
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-07 17:19:29 +0200

    Prepare specs files (#30)
    * keep only relevant routes
    
    * remove old files
    
    * create automated ticket requests skeleton
    
    * prepare the skeleton of the specs

--- spec/requests/automated_tickets/setup_spec.rb
+++ spec/requests/automated_tickets/setup_spec.rb
@@ -1,17 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'AutomatedTickets::Setups', type: :request do
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/automated_tickets/setup/show'
-      expect(response).to have_http_status(:success)
+  describe '/automated_tickets/:automated_ticket_id/setup/:step_name/edit' do
+    context 'GET' do
+      describe 'automated_tickets/setup#edit' do
+        it 'show the edit version of the step'
+      end
     end
   end
-
-  describe 'GET /update' do
-    it 'returns http success' do
-      get '/automated_tickets/setup/update'
-      expect(response).to have_http_status(:success)
+  describe '/automated_tickets/:automated_ticket_id/setup/:step_name' do
+    context 'GET' do
+      describe 'automated_tickets/setup#show' do
+        it 'show the setup version of the step'
+      end
+    end
+    context 'PATCH' do
+      describe 'automated_tickets/setup#update' do
+        it 'updates the automated ticket and redirect to the relevant page'
+      end
+    end
+    context 'PUT' do
+      describe 'automated_tickets/setup#update' do
+        it 'updates the automated ticket and redirect to the relevant page'
+      end
     end
   end
 end
--- spec/requests/automated_tickets_spec.rb
+++ spec/requests/automated_tickets_spec.rb
@@ -1,22 +1,39 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'AutomatedTickets', type: :request do
-  describe 'GET /automated_tickets/new' do
-    it 'returns http success' do
-      get '/automated_tickets/new'
-      expect(response).to have_http_status(:success)
+  describe '/automated_tickets' do
+    context 'GET' do
+      describe 'automated_tickets#index' do
+        it 'returns the index'
+      end
     end
   end
-  describe 'GET /automated_tickets/new' do
-    it 'returns http success' do
-      get '/automated_tickets/new'
-      expect(response).to have_http_status(:success)
+
+  describe '/automated_tickets/new' do
+    context 'GET' do
+      describe 'automated_tickets#new' do
+        it 'returns the automated_ticket wizard'
+      end
     end
   end
-  describe 'PUT /automated_tickets' do
-    it 'returns http success' do
-      get '/automated_tickets/new'
-      expect(response).to have_http_status(:success)
+
+  describe '/automated_tickets/:id' do
+    context 'PATCH' do
+      describe 'automated_tickets#update' do
+        it 'updates the automated_ticket'
+      end
+    end
+    context 'PUT' do
+      describe 'automated_tickets#update' do
+        it 'updates the automated_ticket'
+      end
+    end
+    context 'DELETE' do
+      describe 'automated_tickets#destroy' do
+        it 'destroys the automated_ticket'
+      end
     end
   end
 end
