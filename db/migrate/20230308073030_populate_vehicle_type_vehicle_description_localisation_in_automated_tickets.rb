945b77ea71efaf8d8c24f8f017583556ba9078be
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-07 10:06:10 +0200

    comment only the inside of the migration

--- db/migrate/20230308073030_populate_vehicle_type_vehicle_description_localisation_in_automated_tickets.rb
+++ db/migrate/20230308073030_populate_vehicle_type_vehicle_description_localisation_in_automated_tickets.rb
@@ -1,14 +1,14 @@
-# class PopulateVehicleTypeVehicleDescriptionLocalisationInAutomatedTickets < ActiveRecord::Migration[7.0]
-#   def up
-#     AutomatedTicket.ready.find_each do |automated_ticket|
-#       vehicle_in_client = automated_ticket.service.vehicles.find do |vehicle|
-#         vehicle.license_plate == automated_ticket.license_plate
-#       end
-#       automated_ticket.update_columns(
-#         vehicle_type: vehicle_in_client.vehicle_type,
-#         vehicle_description: vehicle_in_client.vehicle_description,
-#         localisation: 'paris'
-#       )
-#     end
-#   end
-# end
+class PopulateVehicleTypeVehicleDescriptionLocalisationInAutomatedTickets < ActiveRecord::Migration[7.0]
+  def up
+    # AutomatedTicket.ready.find_each do |automated_ticket|
+    #   vehicle_in_client = automated_ticket.service.vehicles.find do |vehicle|
+    #     vehicle.license_plate == automated_ticket.license_plate
+    #   end
+    #   automated_ticket.update_columns(
+    #     vehicle_type: vehicle_in_client.vehicle_type,
+    #     vehicle_description: vehicle_in_client.vehicle_description,
+    #     localisation: 'paris'
+    #   )
+    end
+  end
+end
