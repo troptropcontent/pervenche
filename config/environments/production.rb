2cbe86ff9a3ee46b927e445ccebceaaffdc23143
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-07 15:33:47 +0200

    enable unencrypted data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.active_record.encryption.support_unencrypted_data
+  config.active_record.encryption.support_unencrypted_data = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
