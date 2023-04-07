ae202abf1abf0483763e3a057020a01e888648dc
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-07 17:11:11 +0200

    prepare the skeleton of the specs

--- spec/requests/services_spec.rb
+++ spec/requests/services_spec.rb
@@ -1,10 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Services', type: :request do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/services/new'
-      expect(response).to have_http_status(:success)
+  describe '/services' do
+    context 'POST' do
+      describe 'services#create' do
+        it 'creates a new service'
+      end
+    end
+  end
+  describe '/services/new' do
+    context 'GET' do
+      describe 'services#new' do
+        it 'returns the new service page'
+      end
     end
   end
 end
