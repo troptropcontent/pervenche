538c2bdb37394f05d41377e2b08d95f470a8a75a
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-19 22:53:13 +0200

    create an encryptable module

---
+++ app/lib/encryptable.rb
@@ -0,0 +1,12 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Encryptable
+  extend T::Sig
+
+  sig { params(value: T.untyped, deterministic: T.nilable(T::Boolean)).returns(String) }
+  def hash_secret(value, deterministic: true)
+    salt = deterministic ? Rails.application.credentials.active_record_encryption.secondary_deterministic_key : BCrypt::Engine.generate_salt
+    BCrypt::Engine.hash_secret(value, salt)
+  end
+end
