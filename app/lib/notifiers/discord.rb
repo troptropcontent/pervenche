0f9ef3368913d85f5c0febd5761155af4a467e23
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-21 08:58:54 +0200

    setup discord notification (#41)
    * add discord credentials
    
    * create a github notifiers
    
    * setup discord notifications

---
+++ app/lib/notifiers/discord.rb
@@ -0,0 +1,28 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Notifiers
+  class Discord
+    class << self
+      extend T::Sig
+
+      sig { params(channel: Symbol, content: String).void }
+      def send_message(channel, content)
+        return unless ENV.fetch('PERVENCHE_DISCORD_NOTIFICATION', nil)
+
+        url = Rails.application.credentials.notifiers.discord.webhooks_url.fetch(channel)
+        HttpClient.post(
+          url:,
+          body: { content: content_with_env_prefix(content) }
+        )
+      end
+
+      private
+
+      sig { params(content: String).returns(String) }
+      def content_with_env_prefix(content)
+        "Pervenche (#{Rails.env})\n\n#{content}"
+      end
+    end
+  end
+end
