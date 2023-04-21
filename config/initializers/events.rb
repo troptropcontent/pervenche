9791b6ba6689becb2d6d2a26c1658df74d12f32e
Tom Ecrepont <tomecrepont@gmail.com>
2023-04-21 17:16:12 +0200

    setup sidekiq

--- config/initializers/events.rb
+++ config/initializers/events.rb
@@ -2,14 +2,14 @@
 # frozen_string_literal: true
 
 ActiveSupport::Notifications.subscribe 'users.created' do |_name, _start, _finish, _id, payload|
-  Notifiers::Discord.send_message(
+  Notifiers::Discord.send_message_later(
     :new_users,
     I18n.t('notifications.discord.new_user', email: payload.fetch('email'))
   )
 end
 
 ActiveSupport::Notifications.subscribe 'faraday.errors' do |_name, _start, _finish, _id, payload|
-  Notifiers::Discord.send_message(
+  Notifiers::Discord.send_message_later(
     :errors,
     I18n.t('notifications.discord.faraday.errors',
            method: payload.fetch(:method),
