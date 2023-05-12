8a837979e30f5ab701cf9e0bb4fbc00168bafbea
Tom Ecrepont <tomecrepont@gmail.com>
2023-05-12 18:24:29 +0200

    create an helper that set empty value at the begining of a form to avoid missing params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+# typed: true
+
 module ApplicationHelper
+  def empty_inputs_for(form, record_class, field)
+    input_name = "#{form.object_name}[#{field}]"
+    array_field_column = record_class.columns_hash[field.to_s].sql_type_metadata.sql_type.end_with?('[]')
+    input_name += '[]' if array_field_column
+    form.input(field, input_html: { name: input_name, value: nil }, as: :hidden)
+  end
 end
