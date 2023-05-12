f58858bb4b530e1d2211e8c8f3c77893471cfb07
Tom Ecrepont <tomecrepont@gmail.com>
2023-05-12 09:34:20 +0200

    improve style

--- app/errors/controller_error_manager.rb
+++ app/errors/controller_error_manager.rb
@@ -11,35 +11,38 @@ module ControllerErrorManager
                   ActiveRecord::RecordNotSaved,
                   ActiveRecord::RecordNotDestroyed,
                   ActiveModel::ValidationError do |error|
-        render json: {code: :unprocessable_entity, message: error}, status: :unprocessable_entity
+        render json: { code: :unprocessable_entity, message: error }, status: :unprocessable_entity
       end
 
       ###########
       ### 404 ###
       ###########
       rescue_from ActiveRecord::RecordNotFound do |error|
-        render json: {code: :not_found, message: error}, status: :not_found
+        render json: { code: :not_found, message: error }, status: :not_found
       end
 
       ###########
       ### 403 ###
       ###########
-    
+
       rescue_from CanCan::AccessDenied do
-        render json: {code: :forbidden, message: I18n.t("errors.controller.forbidden.message")}, status: :forbidden
+        render json: { code: :forbidden, message: I18n.t('errors.controller.forbidden.message') }, status: :forbidden
       end
 
       ###########
       ### 400 ###
       ###########
       rescue_from ActionController::BadRequest do |error|
-        render json: {code: :bad_request, message: error}, status: :bad_request
+        render json: { code: :bad_request, message: error }, status: :bad_request
       end
 
       rescue_from ActionController::ParameterMissing do |error|
-        message = I18n.t("errors.controller.parameter_missing.general_message")
-        message = I18n.t("errors.controller.parameter_missing.specific_message", param: error.param) if error.param.present?
-        render json: {code: :parameter_missing, message: }, status: :bad_request
+        message = I18n.t('errors.controller.parameter_missing.general_message')
+        if error.param.present?
+          message = I18n.t('errors.controller.parameter_missing.specific_message',
+                           param: error.param)
+        end
+        render json: { code: :parameter_missing, message: }, status: :bad_request
       end
     end
   end
