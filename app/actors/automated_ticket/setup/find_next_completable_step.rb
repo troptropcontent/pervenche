524d5c2d26b4305679297c77d4be3bb7d766767e
Tom Ecrepont <tomecrepont@gmail.com>
2023-05-16 08:08:48 +0200

    rename previous find next step into find next completable step

---
+++ app/actors/automated_ticket/setup/find_next_completable_step.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AutomatedTicket::Setup::FindNextCompletableStep < Actor
+  input :automated_ticket
+  output :next_step
+  def call
+    self.next_step = AutomatedTicket.setup_steps.keys.find do |step|
+      automated_ticket.setup_step = step
+      automated_ticket.invalid?
+    end
+  end
+end
