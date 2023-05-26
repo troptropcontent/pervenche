6ca63f2050dd10c2090d2b5f79fc70ff40eac8f7
Tom Ecrepont <tomecrepont@gmail.com>
2023-05-26 06:56:16 +0200

    create the first draft of the controller spec

--- spec/requests/automated_tickets/setup_spec.rb
+++ spec/requests/automated_tickets/setup_spec.rb
@@ -27,4 +27,22 @@ RSpec.describe 'AutomatedTickets::Setups', type: :request do
       end
     end
   end
+
+  path '/automated_tickets/:automated_ticket_id/setup/:step_name/reset' do
+    let(:automated_ticket_id) { automated_ticket.id }
+    let(:step_name) { :vehicle }
+
+    let!(:automated_ticket) do
+      FactoryBot.create(:automated_ticket, :with_zipcodes, user:, service:)
+    end
+    let(:user) { FactoryBot.create(:user) }
+    let(:service) do
+      service = FactoryBot.build(:service, user_id: user.id)
+      service.save(validate: false)
+      service
+    end
+    put 'reset' do
+      it_behaves_like 'An authenticated endpoint'
+    end
+  end
 end
