01d6f5813d6bb56c28835991ecac3880a4b9d93e
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-14 17:47:12 +0200

    use mailer as the default queue

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
   config.action_mailer.default_options = { from: 'contact@pervenche.eu' }
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.deliver_later_queue_name = 'mailer'
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
