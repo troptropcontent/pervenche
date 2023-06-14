aa5ddf4fbc9fbda8f87dc09f5b8716a8c7fdf081
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-14 17:45:49 +0200

    remove old Maillable attempt

--- config/initializers/maillable.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-# typed: true
-
-require Rails.root.join('app/lib/maillable')
-
-Maillable.configure do |config|
-  config.mailing_client = :sendgrid
-  config.default_from = ENV['PERVENCHE_MALLABLE_DEFAULT_FROM'] || "contact+#{Rails.env}@pervenche.eu"
-end
