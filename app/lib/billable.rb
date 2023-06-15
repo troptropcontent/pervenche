15a0f5d06cc26d6f5d804916a9a6b43307e2573b
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-15 08:08:11 +0200

    setup the new billable controller and the handle action

--- app/lib/billable.rb
+++ app/lib/billable.rb
@@ -11,6 +11,9 @@ module Billable
     sig { returns(T.nilable(T::Hash[Symbol, T.untyped])) }
     attr_accessor :billing_client_configuration
 
+    sig { returns(T.nilable(String)) }
+    attr_accessor :webhook_token
+
     # rubocop:disable Naming/BlockForwarding, Lint/UnusedMethodArgument
     sig { params(blk: T.proc.params(arg0: T.class_of(Billable)).void).void }
     def configure(&blk)
---
+++ spec/requests/webhooks/billable_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'Webhooks::Billables', type: :request do
+  Sidekiq::Testing.fake!
+  path '/webhooks/billable/:token' do
+    let(:token) { Billable.webhook_token }
+    post 'handle' do
+      response '200', 'OK' do
+        params do
+          { event_type: 'subscription_event',
+            content: { some: 'content' } }
+        end
+
+        describe 'webhook type' do
+          context 'when the web hook type starts with subscription' do
+            let(:expected_job_arguments)  do
+              [
+                ['subscription_event', '{"some":"content"}']
+              ]
+            end
+            it 'triggers a SubscriptionJob' do |example|
+              expect { run example }.to change(Billable::Webhook::SubscriptionJob.jobs, :size).by(1)
+              enqueued_job_arguments = Billable::Webhook::SubscriptionJob.jobs.pluck('args')
+              expect(enqueued_job_arguments).to contain_exactly(*expected_job_arguments)
+            end
+          end
+          context 'when the web hook type starts with something else' do
+            params do
+              { event_type: 'some_event',
+                content: { some: 'content' } }
+            end
+            it 'does not do anything' do |example|
+              expect { run example }.not_to change(Sidekiq::Queues['default'], :size)
+            end
+          end
+        end
+      end
+
+      response '401', 'Unauthorized' do
+        let(:token) { 'toto' }
+        context 'when the token is not valid' do
+          it 'return a 403' do |example|
+            run example
+          end
+        end
+      end
+    end
+  end
+end
