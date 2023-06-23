545bcd611aec8edc7b4e63e9830f378c127db483
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-23 10:40:03 +0200

    raise error if update is not possible

--- app/models/billing/address.rb
+++ app/models/billing/address.rb
@@ -3,6 +3,9 @@
 
 module Billing
   class Address < T::Struct
+    extend ActiveModel::Naming
+    include ActiveModel::Conversion
+
     const :customer_id, String
     prop :last_name, T.nilable(String)
     prop :first_name, T.nilable(String)
@@ -13,20 +16,27 @@ module Billing
     prop :zipcode, T.nilable(String)
     prop :country, T.nilable(String)
 
-    def update(attributes)
-      updated_billing_address_data = Billable::Clients::ChargeBee::Customer.update_billing_address(customer_id,
-                                                                                                   attributes)
+    def update!(attributes)
+      updated_billing_address_data = Billable::Clients::ChargeBee::Customer.update_address(customer_id,
+                                                                                           attributes)
+
+      raise Errors::UnprocessableEntity, 'The address could not been saved' if updated_billing_address_data.nil?
+
       self.last_name = updated_billing_address_data['last_name']
       self.first_name = updated_billing_address_data['first_name']
+      self.address = updated_billing_address_data['line1']
+      self.city = updated_billing_address_data['city']
+      self.company = updated_billing_address_data['company']
+      self.zipcode = updated_billing_address_data['zipcode']
       self
-    end
-
-    def to_partial_path
-      'billing/addresses/address'
     end
 
     def customer
       Customer.find(customer_id)
+    end
+
+    def persisted?
+      true
     end
   end
 end
---
+++ app/models/billing/errors.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Billing
+  module Errors
+    class UnprocessableEntity < StandardError; end
+  end
+end
