c18a90fecf8edce1c405da7ce983735e2df53da7
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-23 08:30:34 +0200

    create the address and payment_method model

--- app/models/billing/address.rb
+++ app/models/billing/address.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
+# typed: strict
 
 module Billing
   class Address < T::Struct
-    const :customer_id, T.nilable(String)
-    const :last_name, T.nilable(String)
-    const :first_name, T.nilable(String)
-    const :company, T.nilable(String)
-    const :phone, T.nilable(String)
-    const :address, T.nilable(String)
-    const :city, T.nilable(String)
-    const :zipcode, T.nilable(String)
-    const :country, T.nilable(String)
+    const :customer_id, String
+    prop :last_name, T.nilable(String)
+    prop :first_name, T.nilable(String)
+    prop :company, T.nilable(String)
+    prop :phone, T.nilable(String)
+    prop :address, T.nilable(String)
+    prop :city, T.nilable(String)
+    prop :zipcode, T.nilable(String)
+    prop :country, T.nilable(String)
+
+    def update(attributes)
+      updated_billing_address_data = Billable::Clients::ChargeBee::Customer.update_billing_address(customer_id,
+                                                                                                   attributes)
+      self.last_name = updated_billing_address_data['last_name']
+      self.first_name = updated_billing_address_data['first_name']
+      self
+    end
   end
 end
--- app/models/billing/customer.rb
+++ app/models/billing/customer.rb
@@ -3,28 +3,27 @@
 module Billing
   class Customer < T::Struct
     class Error < StandardError; end
-    const :billing_client_id, String
+    const :client_id, String
     const :email, String
     const :deleted, T::Boolean
+    prop :customer_data, T::Hash[String, T.untyped]
 
     def self.find(id)
       customer_data = Billable::Clients::ChargeBee::Customer.find(id)
       raise Error, "Customer with id #{id} does not exist" if customer_data.nil?
 
       new(
-        billing_client_id: customer_data.dig('customer', 'id'),
+        client_id: customer_data.dig('customer', 'id'),
         email: customer_data.dig('customer', 'email'),
-        deleted: customer_data.dig('customer', 'deleted')
+        deleted: customer_data.dig('customer', 'deleted'),
+        customer_data:
       )
     end
 
     def billing_address
-      customer_data = Billable::Clients::ChargeBee::Customer.find(billing_client_id)
-      raise Error, "Customer with id #{billing_client_id} does not exist" if customer_data.nil?
-
-      address_data = customer_data.dig('customer', 'billing_address')
+      address_data = customer_data.dig('customer', 'billing_address') || {}
       Address.new(
-        customer_id: billing_client_id,
+        customer_id: client_id,
         last_name: address_data['last_name'],
         first_name: address_data['first_name'],
         company: address_data['company'],
@@ -33,6 +32,19 @@ module Billing
         city: address_data['city'],
         zipcode: address_data['zip'],
         country: address_data['country']
+      )
+    end
+
+    def payment_method
+      payment_method_data = customer_data['card'] || {}
+      PaymentMethod.new(
+        customer_id: client_id,
+        status: payment_method_data['status'],
+        last_four_digits: payment_method_data['last4'],
+        card_type: payment_method_data['card_type'],
+        funding_type: payment_method_data['funding_type'],
+        expiry_month: payment_method_data['expiry_month'],
+        expiry_year: payment_method_data['expiry_year']
       )
     end
   end
