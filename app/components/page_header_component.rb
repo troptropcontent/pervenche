94e91c46101936e7866514b7af75e23ac0e7d12b
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-23 10:38:44 +0200

    create a PageHeader component

--- app/components/page_header_component.rb
+++ app/components/page_header_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PageHeaderComponent < ViewComponent::Base
-  def initialize(title: , subtitle: nil)
+  def initialize(title:, subtitle: nil)
     @title = title
     @subtitle = subtitle
   end
