d7725c8d4c7b4d296a6ef54aa1a634d4ae90899a
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-24 16:34:33 +0200

    create btn component

---
+++ app/components/button_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ButtonComponent < ViewComponent::Base
+  def initialize(path: , method: , text: nil, color: 'primary')
+    @color = color
+    @path = path
+    @method = method
+    @text = text
+  end
+end
