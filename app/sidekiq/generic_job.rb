8308b066272d878361ee5e50ff81c4efaf59eb08
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-27 17:23:41 +0200

    revamp GenericJob

--- app/sidekiq/generic_job.rb
+++ app/sidekiq/generic_job.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 class GenericJob
   include Sidekiq::Job
 
-  def perform(receiver_class, method, arguments, options = {})
-    arguments = [arguments] unless arguments.is_a?(Array)
+  def perform(receiver_class, method, options = {})
+    method_args = options['method_args']
     receiver = receiver_class.constantize
     receiver = receiver.find(options['find_id']) if options['find_id']
     receiver = receiver.new(*options['new_args']) if options['new_args']
-    receiver.send(method, *arguments)
+    if method_args
+      method_args = [method_args] unless method_args.is_a?(Array)
+      receiver.send(method, *method_args)
+    else
+      receiver.send(method)
+    end
   end
 end
