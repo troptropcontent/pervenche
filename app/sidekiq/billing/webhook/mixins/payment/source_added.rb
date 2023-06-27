3c5c3ca7ab70dd9d1b3608fbee6702a897a72576
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-27 17:25:00 +0200

    handle payment_source_added webhooks

---
+++ app/sidekiq/billing/webhook/mixins/payment/source_added.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+# typed: strict
+
+module Billing
+  module Webhook
+    module Mixins
+      module Payment
+        module SourceAdded
+          extend T::Sig
+          sig { params(content: T.untyped).void }
+          def process_source_added_webhook(content)
+            customer = Billing::Customer.find(content.dig('customer', 'id'))
+            subscriptions = customer.subscriptions
+
+            subscriptions.each do |subscription|
+              # next if [subscription.status, subscription.cancel_reason] != %w[cancelled no_card]
+              next if subscription.status != 'cancelled'
+
+              GenericJob.perform_async(
+                'Billing::Subscription',
+                'reactivate',
+                { 'find_id' => subscription.client_id }
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- app/sidekiq/billing/webhook/payment_job.rb
+++ app/sidekiq/billing/webhook/payment_job.rb
@@ -1,7 +1,16 @@
-class Billing::Webhook::PaymentJob
-  include Sidekiq::Job
-
-  def perform(*args)
-    # Do something
+module Billing
+  module Webhook
+    class PaymentJob
+      include Sidekiq::Job
+      include Mixins::Payment::SourceAdded
+      def perform(webhook_type, serialized_content)
+        action = webhook_type.delete_prefix('payment_')
+        content = JSON.parse(serialized_content)
+        case action
+        when 'source_added'
+          process_source_added_webhook(content)
+        end
+      end
+    end
   end
 end
