2ead8bf143846e682c47ea0872037d36415c176b
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-30 10:30:58 +0200

    allow admin only roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,29 +7,31 @@ class Ability
     return unless user.present?
     # all common signed in abilities goes here
 
-    return unless user.has_role?('customer')
-
-    # all customer abilities goes here
-    can %i[new], AutomatedTicket
-    can %i[show reset], AutomatedTicket, { user:, status: %i[started setup] }
-    can %i[update], AutomatedTicket, { user: }
-    can %i[index destroy], AutomatedTicket, { user_id: user.id, status: :ready }
-    can %i[new create update], Service, { user_id: user.id }
-    can %i[show], Billing::Customer do |customer|
-      user.chargebee_customer_id == customer.client_id
+    if user.has_role?('customer')
+      # all customer abilities goes here
+      can %i[new], AutomatedTicket
+      can %i[show reset], AutomatedTicket, { user:, status: %i[started setup] }
+      can %i[update], AutomatedTicket, { user: }
+      can %i[index destroy], AutomatedTicket, { user_id: user.id, status: :ready }
+      can %i[new create update], Service, { user_id: user.id }
+      can %i[show], Billing::Customer do |customer|
+        user.chargebee_customer_id == customer.client_id
+      end
+      can %i[edit update], Billing::Address do |address|
+        user.chargebee_customer_id == address.customer.client_id
+      end
+      can %i[destroy], Billing::Subscription, customer_id: user.chargebee_customer_id
     end
-    can %i[edit update], Billing::Address do |address|
-      user.chargebee_customer_id == address.customer.client_id
-    end
-    can %i[destroy], Billing::Subscription, customer_id: user.chargebee_customer_id
-    return unless user.has_role?('admin')
 
-    # all admin abilities goes here
-    can %i[dashboard], :admin
-    can %i[show], Admin::Diagnostics::Client
-    can %i[show], Admin::Diagnostics::TicketsToRenew
-    can %i[show index destroy], Billing::Subscription
-    can %i[show], Billing::Customer
-    can %i[edit update], Billing::Address
+    if user.has_role?('admin')      
+      # all admin abilities goes here
+      can %i[dashboard], :admin
+      can %i[show], Admin::Diagnostics::Client
+      can %i[show], Admin::Diagnostics::TicketsToRenew
+      can %i[show index destroy], Billing::Subscription
+      can %i[show], Billing::Customer
+      can %i[edit update], Billing::Address
+      can %i[export], AutomatedTicket 
+    end
   end
 end
