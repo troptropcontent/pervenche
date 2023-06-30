7642c70e32a00f0b0fab103c5da917b470e41163
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-30 10:33:29 +0200

    export endpoint (#148)
    * handle authenticated endpoint with non html format
    
    * allow admin only roles
    
    * allow admin only roles
    
    * allow admin only roles
    
    * fix automated ticket factory
    
    * handle authenticated endpoint with non html format
    
    * release the export endpoint

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,23 +5,25 @@ class Ability
 
   def initialize(user)
     return unless user.present?
+
     # all common signed in abilities goes here
 
-    return unless user.has_role?('customer')
+    if user.has_role?('customer')
+      # all customer abilities goes here
+      can %i[new], AutomatedTicket
+      can %i[show reset], AutomatedTicket, { user:, status: %i[started setup] }
+      can %i[update], AutomatedTicket, { user: }
+      can %i[index destroy], AutomatedTicket, { user_id: user.id, status: :ready }
+      can %i[new create update], Service, { user_id: user.id }
+      can %i[show], Billing::Customer do |customer|
+        user.chargebee_customer_id == customer.client_id
+      end
+      can %i[edit update], Billing::Address do |address|
+        user.chargebee_customer_id == address.customer.client_id
+      end
+      can %i[destroy], Billing::Subscription, customer_id: user.chargebee_customer_id
+    end
 
-    # all customer abilities goes here
-    can %i[new], AutomatedTicket
-    can %i[show reset], AutomatedTicket, { user:, status: %i[started setup] }
-    can %i[update], AutomatedTicket, { user: }
-    can %i[index destroy], AutomatedTicket, { user_id: user.id, status: :ready }
-    can %i[new create update], Service, { user_id: user.id }
-    can %i[show], Billing::Customer do |customer|
-      user.chargebee_customer_id == customer.client_id
-    end
-    can %i[edit update], Billing::Address do |address|
-      user.chargebee_customer_id == address.customer.client_id
-    end
-    can %i[destroy], Billing::Subscription, customer_id: user.chargebee_customer_id
     return unless user.has_role?('admin')
 
     # all admin abilities goes here
@@ -31,5 +33,6 @@ class Ability
     can %i[show index destroy], Billing::Subscription
     can %i[show], Billing::Customer
     can %i[edit update], Billing::Address
+    can %i[export], AutomatedTicket
   end
 end
