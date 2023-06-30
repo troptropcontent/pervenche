5ce69f922d01bfdeebf85a317fed35ccc50f58ba
Tom Ecrepont <tomecrepont@gmail.com>
2023-06-30 10:31:42 +0200

    handle authenticated endpoint with non html format

--- spec/requests/shared_example/an_authenticated_endpoint.rb
+++ spec/requests/shared_example/an_authenticated_endpoint.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'An authenticated endpoint' do
-  response '302', 'Found' do
-    it 'redirect to the log in page' do |example|
-      run(example)
-      expect(response).to redirect_to('/users/sign_in')
+  if example.metadata[:path].to_s.match(/.*(\.(.*))/) && example.metadata[:path].to_s.match(/.*(\.(.*))/)[1] != '.html'
+    response '401', 'Unauthorized' do
+      it 'returns a 401 Unauthorised' do |example|
+        run(example)
+      end
+    end
+  else
+    response '302', 'Found' do
+      it 'redirect to the log in page' do |example|
+        run(example)
+        expect(response).to redirect_to('/users/sign_in')
+      end
     end
   end
 end
