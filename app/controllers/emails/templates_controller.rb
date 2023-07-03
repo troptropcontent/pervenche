335b09883441ea84814981a620f98fc3b00a12b0
Tom Ecrepont <tomecrepont@gmail.com>
2023-07-03 17:48:33 +0200

    load mailers if not there yet

--- app/controllers/emails/templates_controller.rb
+++ app/controllers/emails/templates_controller.rb
@@ -2,10 +2,20 @@
 
 module Emails
   class TemplatesController < ApplicationController
-    def index; end
+    def index
+      load_mailers if TemplateMailer.descendants.empty?
+      @mailers = TemplateMailer.descendants
+    end
 
     def show; end
 
     def deliver; end
+
+    private
+
+    def load_mailers
+      mailers = Dir[Rails.root.join('app/mailers/**/*.rb')]
+      mailers.each { |file| require file }
+    end
   end
 end
