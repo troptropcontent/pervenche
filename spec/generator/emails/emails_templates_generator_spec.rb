6d39f54a4bb4e35a325863782c8e653b3264fbfb
Tom Ecrepont <tomecrepont@gmail.com>
2023-07-07 16:33:52 +0200

    create the generator and add this to the admin links

--- spec/generator/emails/emails_templates_generator_spec.rb
+++ spec/generator/emails/emails_templates_generator_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Emails::Templates", type: :generator do
-
+RSpec.describe 'Emails::Templates', type: :generator do
   pending "add some scenarios (or delete) #{__FILE__}"
 end
