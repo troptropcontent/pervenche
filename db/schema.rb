e7d16865758a097cbab55467c185d0e0083219b6
Tom Ecrepont <tomecrepont@gmail.com>
2023-08-14 07:40:31 +0200

    setup Noticed

--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_610_062_942) do
+ActiveRecord::Schema[7.0].define(version: 20_230_813_074_121) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "automated_tickets", force: :cascade do |t|
-    t.bigint "service_id"
-    t.bigint "user_id", null: false
-    t.string "type"
-    t.string "rate_option_client_internal_id"
-    t.string "license_plate"
-    t.integer "status", default: 0, null: false
-    t.boolean "active", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "weekdays", array: true
-    t.string "accepted_time_units", array: true
-    t.string "zipcodes", default: [], array: true
-    t.string "payment_method_client_internal_ids", array: true
-    t.string "vehicle_type"
-    t.string "vehicle_description"
-    t.string "localisation"
-    t.boolean "free", default: false
-    t.string "charge_bee_subscription_id"
-    t.integer "kind"
-    t.index ["service_id"], name: "index_automated_tickets_on_service_id"
-    t.index ["user_id"], name: "index_automated_tickets_on_user_id"
+  create_table 'automated_tickets', force: :cascade do |t|
+    t.bigint 'service_id'
+    t.bigint 'user_id', null: false
+    t.string 'type'
+    t.string 'rate_option_client_internal_id'
+    t.string 'license_plate'
+    t.integer 'status', default: 0, null: false
+    t.boolean 'active', default: false, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'weekdays', array: true
+    t.string 'accepted_time_units', array: true
+    t.string 'zipcodes', default: [], array: true
+    t.string 'payment_method_client_internal_ids', array: true
+    t.string 'vehicle_type'
+    t.string 'vehicle_description'
+    t.string 'localisation'
+    t.boolean 'free', default: false
+    t.string 'charge_bee_subscription_id'
+    t.integer 'kind'
+    t.index ['service_id'], name: 'index_automated_tickets_on_service_id'
+    t.index ['user_id'], name: 'index_automated_tickets_on_user_id'
   end
 
-  create_table "automated_tickets_setups", force: :cascade do |t|
-    t.bigint "service_id", null: false
-    t.string "localisation"
-    t.string "vehycle_type"
-    t.string "license_plate"
-    t.integer "weekdays", default: [1, 2, 3, 4, 5, 6, 0], array: true
-    t.string "payment_method_client_internal_ids", array: true
-    t.string "rate_option_client_internal_id"
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["service_id"], name: "index_automated_tickets_setups_on_service_id"
+  create_table 'automated_tickets_setups', force: :cascade do |t|
+    t.bigint 'service_id', null: false
+    t.string 'localisation'
+    t.string 'vehycle_type'
+    t.string 'license_plate'
+    t.integer 'weekdays', default: [1, 2, 3, 4, 5, 6, 0], array: true
+    t.string 'payment_method_client_internal_ids', array: true
+    t.string 'rate_option_client_internal_id'
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['service_id'], name: 'index_automated_tickets_setups_on_service_id'
   end
 
   create_table 'blazer_audits', force: :cascade do |t|
@@ -108,6 +108,18 @@ ActiveRecord::Schema[7.0].define(version: 20_230_610_062_942) do
     t.index ['creator_id'], name: 'index_blazer_queries_on_creator_id'
   end
 
+  create_table 'notifications', force: :cascade do |t|
+    t.string 'recipient_type', null: false
+    t.bigint 'recipient_id', null: false
+    t.string 'type', null: false
+    t.jsonb 'params'
+    t.datetime 'read_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['read_at'], name: 'index_notifications_on_read_at'
+    t.index %w[recipient_type recipient_id], name: 'index_notifications_on_recipient'
+  end
+
   create_table 'robots', force: :cascade do |t|
     t.bigint 'service_id', null: false
     t.string 'license_plate', null: false
@@ -121,66 +133,66 @@ ActiveRecord::Schema[7.0].define(version: 20_230_610_062_942) do
     t.index ['service_id'], name: 'index_robots_on_service_id'
   end
 
-  create_table "services", force: :cascade do |t|
-    t.string "integer", default: "0", null: false
-    t.string "name"
-    t.string "username", null: false
-    t.string "password", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.integer "kind"
-    t.index ["user_id"], name: "index_services_on_user_id"
-    t.index ["username", "kind"], name: "index_services_on_username_and_kind", unique: true
+  create_table 'services', force: :cascade do |t|
+    t.string 'integer', default: '0', null: false
+    t.string 'name'
+    t.string 'username', null: false
+    t.string 'password', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id', null: false
+    t.integer 'kind'
+    t.index ['user_id'], name: 'index_services_on_user_id'
+    t.index %w[username kind], name: 'index_services_on_username_and_kind', unique: true
   end
 
-  create_table "ticket_requests", force: :cascade do |t|
-    t.bigint "automated_ticket_id", null: false
-    t.string "payment_method_client_internal_id"
-    t.datetime "requested_on", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "quantity", null: false
-    t.string "time_unit", null: false
-    t.string "zipcode"
-    t.index ["automated_ticket_id"], name: "index_ticket_requests_on_automated_ticket_id"
+  create_table 'ticket_requests', force: :cascade do |t|
+    t.bigint 'automated_ticket_id', null: false
+    t.string 'payment_method_client_internal_id'
+    t.datetime 'requested_on', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'quantity', null: false
+    t.string 'time_unit', null: false
+    t.string 'zipcode'
+    t.index ['automated_ticket_id'], name: 'index_ticket_requests_on_automated_ticket_id'
   end
 
-  create_table "tickets", force: :cascade do |t|
-    t.datetime "starts_on", null: false
-    t.datetime "ends_on", null: false
-    t.string "license_plate", null: false
-    t.integer "cost_cents", null: false
-    t.string "client_internal_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "automated_ticket_id", null: false
-    t.string "zipcode"
-    t.index ["automated_ticket_id"], name: "index_tickets_on_automated_ticket_id"
+  create_table 'tickets', force: :cascade do |t|
+    t.datetime 'starts_on', null: false
+    t.datetime 'ends_on', null: false
+    t.string 'license_plate', null: false
+    t.integer 'cost_cents', null: false
+    t.string 'client_internal_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'automated_ticket_id', null: false
+    t.string 'zipcode'
+    t.index ['automated_ticket_id'], name: 'index_tickets_on_automated_ticket_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "roles", default: ["customer"], null: false, array: true
-    t.string "chargebee_customer_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["roles"], name: "index_users_on_roles", using: :gin
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'provider'
+    t.string 'uid'
+    t.string 'roles', default: ['customer'], null: false, array: true
+    t.string 'chargebee_customer_id'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['roles'], name: 'index_users_on_roles', using: :gin
   end
 
-  add_foreign_key "automated_tickets", "services"
-  add_foreign_key "automated_tickets", "users"
-  add_foreign_key "automated_tickets_setups", "services"
-  add_foreign_key "robots", "services"
-  add_foreign_key "services", "users"
-  add_foreign_key "ticket_requests", "automated_tickets"
-  add_foreign_key "tickets", "automated_tickets"
+  add_foreign_key 'automated_tickets', 'services'
+  add_foreign_key 'automated_tickets', 'users'
+  add_foreign_key 'automated_tickets_setups', 'services'
+  add_foreign_key 'robots', 'services'
+  add_foreign_key 'services', 'users'
+  add_foreign_key 'ticket_requests', 'automated_tickets'
+  add_foreign_key 'tickets', 'automated_tickets'
 end
